80c8efba79c94513d86376e30e46af47a1002eea
handsome-bit <3025572773@qq.com>
2025-12-07 20:53:48 +0800

    1、增加缓冲区刷新操作，防止串口出现无效字符

--- src/modbus.c
+++ src/modbus.c
@@ -411,6 +411,7 @@ int _modbus_receive_msg(modbus_t *ctx, uint8_t *msg, msg_type_t msg_type)
         p_tv = &tv;
     }
 
+    modbus_flush(ctx);
     while (length_to_read != 0) {
         rc = ctx->backend->select(ctx, &rset, p_tv, length_to_read);
         if (rc == -1) {
